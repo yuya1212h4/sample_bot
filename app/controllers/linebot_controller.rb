081399e226c31e8246ca0f829a7d1ff55f335cee
yuya1212h4 <yuya1212h4.066@gmail.com>
2019-11-28 16:11:55 +0900

    環境変数修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -5,7 +5,7 @@ class LinebotController < ApplicationController
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_ID = ENV["LINE_CHANNEL_ID"]
     }
   end
 
