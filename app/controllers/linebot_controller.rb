0fd539d945f6b4aea25f3465d0f1421953592275
yuya1212h4 <yuya1212h4.066@gmail.com>
2019-11-28 18:20:14 +0900

    カルーセル

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -114,6 +114,75 @@ class LinebotController < ApplicationController
           }
           p message
           client.reply_message(event['replyToken'], message)
+        elsif event.message['text'] == "カルーセル"
+          message = {
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                      # "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+                      "imageBackgroundColor": "#FFFFFF",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/123"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=111"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=111"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                               "uri": "http://example.com/page/111"
+                          }
+                      ]
+                    },
+                    {
+                      # "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+                      "imageBackgroundColor": "#000000",
+                      "title": "this is menu",
+                      "text": "description",
+                      "defaultAction": {
+                          "type": "uri",
+                          "label": "View detail",
+                          "uri": "http://example.com/page/222"
+                      },
+                      "actions": [
+                          {
+                              "type": "postback",
+                              "label": "Buy",
+                              "data": "action=buy&itemid=222"
+                          },
+                          {
+                              "type": "postback",
+                              "label": "Add to cart",
+                              "data": "action=add&itemid=222"
+                          },
+                          {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                          }
+                      ]
+                    }
+                ],
+                "imageAspectRatio": "rectangle",
+                "imageSize": "cover"
+            }
+          }
+          client.reply_message(event['replyToken'], message)
         else
           case event.type
           when Line::Bot::Event::MessageType::Text
